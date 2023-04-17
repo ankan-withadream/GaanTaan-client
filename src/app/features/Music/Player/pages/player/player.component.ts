d3d24bbd0f5bd84840527d5c63608f96a8ed00d7
ankan-withadream <ankanwithadream@gmail.com>
2023-04-17 13:11:28 +0530

    Asynchronus song loading on play button click

--- src/app/features/Music/Player/pages/player/player.component.ts
+++ src/app/features/Music/Player/pages/player/player.component.ts
@@ -17,67 +17,83 @@ export class PlayerComponent implements OnInit {
 
   constructor(
     private http: HttpClient,
-  ) { 
+  ) {
 
   }
 
   ngOnInit(): void {
 
-    // const audioUrl = 'https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3';
+  }
+
+  async load_song() {
     const audioUrl = "http://localhost:3000/stream";
     const headers = {
-       'Content-Type': 'audio/mpeg',
-       'Access-Control-Allow-Origin': '*',
-      };
-    // this.http.get(audioUrl, { headers, responseType: 'blob' }).subscribe(response => {
-    //   const audioBlob = new Blob([response], { type: 'audio/mpeg' });
-    //   const audioUrl = URL.createObjectURL(audioBlob);
-    //   console.log(audioUrl);
-    //   // You can now use this audio URL to play the audio in an audio player component.
-    // });
+      'Content-Type': 'audio/mpeg',
+      'Access-Control-Allow-Origin': '*',
+    };
 
-    this.http.get(audioUrl, { headers, responseType: 'blob' }).subscribe((audioBlob: Blob) => {
-      const audioUrl = URL.createObjectURL(audioBlob);
-      this.audioPlayer = new Audio(audioUrl);
-      this.audioPlayer.addEventListener('loadedmetadata', () => {
-        this.audioDuration = this.audioPlayer.duration;
-      });
-      this.audioPlayer.addEventListener('timeupdate', () => {
-        this.currentTime = this.audioPlayer.currentTime;
-      });
-    });
+    return new Promise(async (resolve, reject) => {
+      const that = this;
+      this.http.get(audioUrl, { headers, responseType: 'blob' }).subscribe({
+        next(audioBlob: Blob) {
+          const audioUrl = URL.createObjectURL(audioBlob);
+          that.audioPlayer = new Audio(audioUrl);
+          resolve(that.audioPlayer);
+          that.audioPlayer.addEventListener('loadedmetadata', () => {
+            that.audioDuration = that.audioPlayer.duration;
+          });
+          that.audioPlayer.addEventListener('timeupdate', () => {
+            that.currentTime = that.audioPlayer.currentTime;
+          });
+        },
+
+        error(error) {
+          reject(error);
+        },
+      }
+
+      );
+    })
+
   }
 
 
 
-play() {
-  console.log("play clicked", );
-  if (this.audioPlayer) {
-    this.audioPlayer.play();
-    this.isPlaying = true;
+  async play() {
+    console.log("play clicked",);
+    await this.load_song().then((audioPlayer) => {
+      console.log("audioPlayer", audioPlayer);
+      this.audioPlayer.play();
+      this.isPlaying = true;
+    }).catch((error) => {
+      console.log("error", error);
+    })
+    // if (this.audioPlayer) {
+    //   this.audioPlayer.play();
+    //   this.isPlaying = true;
+    // }
   }
-}
 
-pause() {
-  if (this.audioPlayer) {
-    this.audioPlayer.pause();
-    this.isPlaying = false;
+  pause() {
+    if (this.audioPlayer) {
+      this.audioPlayer.pause();
+      this.isPlaying = false;
+    }
   }
-}
 
-seek(timeEvent: any) {
-  let time: number = timeEvent.target.value
-  console.log("time got", time);
-  console.log("audioplayer", this.audioPlayer);
-  if (this.audioPlayer) {
-    this.audioPlayer.currentTime = time;
+  seek(timeEvent: any) {
+    let time: number = timeEvent.target.value
+    console.log("time got", time);
+    console.log("audioplayer", this.audioPlayer);
+    if (this.audioPlayer) {
+      this.audioPlayer.currentTime = time;
+    }
   }
-}
 
-formatTime(time: number): string {
-  const minutes = Math.floor(time / 60);
-  const seconds = Math.floor(time % 60);
-  return `${minutes}:${seconds.toString().padStart(2, '0')}`;
-}
+  formatTime(time: number): string {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    return `${minutes}:${seconds.toString().padStart(2, '0')}`;
+  }
 
 }
