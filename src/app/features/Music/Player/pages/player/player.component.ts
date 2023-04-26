0178074b02a06f84a406e0b9a991a2502a01c812
ankan-withadream <ankanwithadream@gmail.com>
2023-04-26 19:23:26 +0530

    Initial audio uploading is working

--- src/app/features/Music/Player/pages/player/player.component.ts
+++ src/app/features/Music/Player/pages/player/player.component.ts
@@ -1,35 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-
+import { HttpClient, HttpHeaders, HttpRequest } from '@angular/common/http';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-player',
   templateUrl: './player.component.html',
-  styleUrls: ['./player.component.scss']
+  styleUrls: ['./player.component.scss'],
 })
 export class PlayerComponent implements OnInit {
-
   // audioPlayer: HTMLAudioElement;
   audioPlayer: any;
-  isPlaying = false;
+  isPlaying: boolean = false;
   audioDuration: number = 0;
-  currentTime = 0;
+  currentTime: number = 0;
+
+  // fileToUpload: File = null;
+  fileToUpload: any;
+  fileFormGroup : any;
 
   constructor(
     private http: HttpClient,
-  ) {
+    private formBuilder: FormBuilder,
+    ) {
 
-  }
+    }
 
   ngOnInit(): void {
-
+    this.fileFormGroup = this.formBuilder.group({
+      fileForm: ["", Validators.required]
+    })
   }
 
   async load_song() {
-    const audioUrl = "http://localhost:3000/stream";
+    const audioUrl = 'http://localhost:3000/stream';
     const headers = {
       'Content-Type': 'audio/mpeg',
-      'Access-Control-Allow-Origin': '*',
+      // 'Access-Control-Allow-Origin': '*',
     };
 
     return new Promise(async (resolve, reject) => {
@@ -50,28 +56,24 @@ export class PlayerComponent implements OnInit {
         error(error) {
           reject(error);
         },
-      }
-
-      );
-    })
-
+      });
+    });
   }
 
-
-
   async play() {
-    console.log("play clicked",);
-    await this.load_song().then((audioPlayer) => {
-      console.log("audioPlayer", audioPlayer);
-      this.audioPlayer.play();
-      this.isPlaying = true;
-    }).catch((error) => {
-      console.log("error", error);
-    })
-    // if (this.audioPlayer) {
-    //   this.audioPlayer.play();
-    //   this.isPlaying = true;
-    // }
+    console.log('play clicked');
+    if (this.audioPlayer == undefined) {
+      // load song metadata
+      await this.load_song()
+        .then((audioPlayer) => {
+          console.log('audioPlayer', audioPlayer);
+        })
+        .catch((error) => {
+          console.log('error', error);
+        });
+    }
+    this.audioPlayer.play();
+    this.isPlaying = true;
   }
 
   pause() {
@@ -82,9 +84,9 @@ export class PlayerComponent implements OnInit {
   }
 
   seek(timeEvent: any) {
-    let time: number = timeEvent.target.value
-    console.log("time got", time);
-    console.log("audioplayer", this.audioPlayer);
+    let time: number = timeEvent.target.value;
+    console.log('time got', time);
+    console.log('audioplayer', this.audioPlayer);
     if (this.audioPlayer) {
       this.audioPlayer.currentTime = time;
     }
@@ -96,4 +98,45 @@ export class PlayerComponent implements OnInit {
     return `${minutes}:${seconds.toString().padStart(2, '0')}`;
   }
 
+
+  handleFileInput(files: any) {
+    let file = files.target.files;
+    this.fileToUpload = file[0];
+    console.log("File selected: ", this.fileToUpload);
+  }
+
+  uploadFileToServer() {
+    console.log("File selected to upload: ", this.fileToUpload);
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+          "Content-Type": "*",
+      })
+  };
+    const endpoint = 'http://localhost:3000/upload-audio';
+    let formData: any = new FormData();
+    // formData.append('audioUrl', this.fileToUpload );
+    // formData.append('filename', this.fileToUpload.name );
+    formData.append('audioFile', this.fileToUpload, this.fileToUpload.name);
+    for (var pair of formData.entries()) {
+      console.log(pair[0]+ ', ' + pair[1]); 
+  }
+    console.log("bofore request, formdata:", formData.values());
+    // let data = {
+    //   filename: this.fileToUpload.name,
+    //   audioFile: this.fileToUpload
+    // }
+
+    // const req = new HttpRequest('OPTIONS', endpoint, httpOptions)
+  // console.log("fileForm: ", this.fileFormGroup.value);
+  //   let data = {
+  //     "filename": this.fileToUpload.name,
+  //     "audioFile": this.fileFormGroup.value
+  //   }
+
+    this.http.post(endpoint, formData).subscribe(
+      (response) => console.log(response),
+      (error) => console.log(error)
+    );
+  }
 }
